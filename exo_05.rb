73f963fb59252d5b8fc532be50dee11a7387c168
younessibra <youness.ibra@gmail.com>
2020-01-14 11:38:16 +0100

    exo 5

---
+++ exo_05.rb
@@ -0,0 +1,21 @@
+puts "On va compter le nombre d'heures de travail à THP" #affiche le string
+puts "Travail : #{10 * 5 * 11}" #affiche le string tout en évaluant l'expression "10*5*11"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #meme chose que plus haut
+
+puts "Et en secondes ?" #affiche le string
+
+puts 10 * 5 * 11 * 60 * 60 #fait l'opération puis affiche le résultat
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts 3 + 2 < 5 - 7 #affiche false car 5 > -2
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #evalue 3+2 puis l'affiche
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #evalue 5-7 puis l'affiche
+
+puts "Ok, c'est faux alors !" #affiche ce string
+
+puts "C'est drôle ça, faisons-en plus :" #æffiche ce string
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #affiche true car 5>-2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #affiche true car 5 >= -2
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #affiche false car 5 > -2
