c4a9a99c803d1cbdde71f5c529c287bf1ff0ea41
younessibra <youness.ibra@gmail.com>
2020-01-14 11:48:09 +0100

    exo 7

---
+++ exo_07_b.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp #attends que l'utilisateur tape son nom
+puts user_name
---
+++ exo_07_c.rb
@@ -0,0 +1,2 @@
+user_name = gets.chomp #attend que l'utilisateur tape quelque chose
+puts user_name
